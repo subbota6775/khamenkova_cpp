58e1a3ae5feed6106feb54a095a614e670a76156
subbota6775 <98962637+subbota6775@users.noreply.github.com>
2022-02-03 20:03:44 +0300

    Add files via upload

---
+++ TASKS/Task2/Input_name.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+
+using namespace std;
+
+void main()
+{
+    system("chcp 1251 >> null");   
+
+    const string name = "Вячеслав";
+    string input_name;
+    cout << "Введите ваше имя" << endl;
+    cin >> input_name;
+
+    if (input_name == name)
+    {
+        cout << "Привет, " << input_name;
+    }
+    else
+    {
+        cout << "Нет такого имени ";
+    }
+}
