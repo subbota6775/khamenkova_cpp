a892f6eb841cd5f804bb0747f0dc31e0bc04992c
subbota6775 <98962637+subbota6775@users.noreply.github.com>
2022-02-03 20:04:49 +0300

    Add files via upload

---
+++ TASKS/Task3/Array.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+void main()
+{
+    setlocale(LC_ALL, "Rus");
+   
+    cout << "Введите размер массива "; 
+    int length,i;
+    cin >> length;
+
+    int* array = new int[length]; 
+    for (i = 0;i < length;i++)
+    {
+        cout << "Элемент массива " << i + 1 << " = ";
+        cin >> array[i];
+    }
+    cout << "\nЭлементы массива кратных трем = ";
+    int count = 0;
+    for (i = 0;i < length;i++)
+    {
+        if (array[i] % 3 == 0)
+        {
+            cout << array[i] << " ";
+            count++;
+        }
+
+    }
+    cout << "\nКоличество элементов кратных трем = " << count;
+}
