9202c896bd6af06b7a1528293991e97fb1035d52
subbota6775 <98962637+subbota6775@users.noreply.github.com>
2022-02-04 10:08:05 +0300

    Add files via upload

---
+++ TASKS/main/Tasks_main.cpp
@@ -0,0 +1,55 @@
+﻿#include<iostream> 
+#include<math.h>
+
+using namespace std;
+
+void main()
+{
+    system("chcp 1251 >> null");
+
+    int a;
+    cout << "Введите число" << endl;
+    cin >> a;
+
+    if (a > 7)
+    {
+        cout << "Привет!" << endl;
+    }
+
+    const string name = "Вячеслав";
+    string input_name;
+    cout << "\nВведите ваше имя" << endl;
+    cin >> input_name;
+
+    if (input_name == name)
+    {
+        cout << "Привет, " << input_name;
+    }
+    else
+    {
+        cout << "Нет такого имени ";
+    }
+
+    cout << "\n\nВведите размер массива ";
+    int length, i;
+    cin >> length;
+
+    int* array = new int[length];
+    for (i = 0;i < length;i++)
+    {
+        cout << "Элемент массива " << i + 1 << " = ";
+        cin >> array[i];
+    }
+    cout << "\nЭлементы массива кратных трем = ";
+    int count = 0;
+    for (i = 0;i < length;i++)
+    {
+        if (array[i] % 3 == 0)
+        {
+            cout << array[i] << " ";
+            count++;
+        }
+
+    }
+    cout << "\nКоличество элементов кратных трем = " << count;
+}
